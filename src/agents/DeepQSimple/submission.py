6247e98b6c8c1e685c0fc5d8aa0dfa7297410ac7
Alex To <tonhudung@gmail.com>
2020-08-19 00:42:55 +1000

    added DeepQSimple baseline

---
+++ src/agents/DeepQSimple/submission.py
@@ -0,0 +1,20 @@
+import os
+from .DeepQ_NNParam import DeepQ_NNParam
+from .DeepQSimple import DeepQSimple
+
+name = "DeepQSimple"
+
+
+def make_agent(env, submission_dir):
+    """
+    This function will be used by codalab to create your agent. It should accept exactly an environment and a path
+    to your sudmission directory and return a valid agent.
+    """
+    path_model, path_target_model = DeepQ_NNParam.get_path_model(submission_dir, name)
+    nn_archi = DeepQ_NNParam.from_json(os.path.join(path_model, "nn_architecture.json"))
+    res = DeepQSimple(env.action_space,
+                      name=name,
+                      nn_archi=nn_archi,
+                      observation_space=env.observation_space)
+    res.load(submission_dir)
+    return res
